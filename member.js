0a9a7d0d0e08bfc127f3bb88c25d514d4b0efb3e
Dark <103580402+Gigio42@users.noreply.github.com>
2024-03-11 17:39:59 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember() {
+  console.log('I am a member of the team and I have the following skills: ')
+  console.log('I am a good listener')
+  console.log('I am a good communicator')
+  console.log('I am a good team player')
+  console.log('I am a good problem solver')
+  console.log('I am a good time manager')
+}
